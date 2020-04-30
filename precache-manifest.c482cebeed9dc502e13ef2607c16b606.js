19d8d0afaa0053cdaccd84338595eb5e17056c8a
hypocrisy-ywk <1145026809@qq.com>
2020-04-30 19:49:10 +0800

    deploy

---
+++ precache-manifest.c482cebeed9dc502e13ef2607c16b606.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51aac9e8b227ff83204d",
+    "url": "/account/css/app.d3ec91aa.css"
+  },
+  {
+    "revision": "8003034383d39b3270bd",
+    "url": "/account/css/chunk-vendors.87d052e1.css"
+  },
+  {
+    "revision": "4f1a5d6c91271fe90f5d98402ff2aa87",
+    "url": "/account/index.html"
+  },
+  {
+    "revision": "51aac9e8b227ff83204d",
+    "url": "/account/js/app.b7b82267.js"
+  },
+  {
+    "revision": "8003034383d39b3270bd",
+    "url": "/account/js/chunk-vendors.63642860.js"
+  },
+  {
+    "revision": "4c19ae82e94d2bbdf641062559953321",
+    "url": "/account/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/account/robots.txt"
+  }
+]);
